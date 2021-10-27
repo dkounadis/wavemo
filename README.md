19af5c99a1815b4c9feb9fe016532cc40509741d
Dionyssos Koudanis-Bastian <dionyssos.kounadis-bastian@smartmeup.org>
2021-10-27 14:24:28 +0200

    update readme

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ End-to-end emotion recognition from raw audio
 A simple Convolutional Neural Network for emotion recognition using the Berlin Database of Emotional Speech EMODB.
 You can download EMODB here http://emodb.bilderbar.info/download/
 
-You can train from scratch using `train.py` (full train ~17mins in an RTX GPU)
-or you can download a pretrained model here https://drive.google.com/file/d/11h4XOqM2pvaPj9MhVCwGJmXKqWZUswYE/view?usp=sharing
+Run `train.py` (full train ~17mins in an RTX GPU) to train a model from scratch
+or download a pretrained model (36MB) here https://drive.google.com/file/d/11h4XOqM2pvaPj9MhVCwGJmXKqWZUswYE/view?usp=sharing
+
+Run `jupyter notebook test.ipynb` to predict emotion on your own voice recording (must be 16kHZ `.wav`)
--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import torch
 
 cfg = PermissiveDict()
 cfg.dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-cfg.data_dir = 'emodb-src/'  # path to EMODB 
+cfg.data_dir = 'emodb-src/'  # path to EMODB http://emodb.bilderbar.info/download/
 cfg.net_dir = './networks/'
-cfg.best_pth = 'epoch_138_val_speaker_[15]_accuracy_0000,76786.pth'
+
+cfg.best_pth = 'epoch_138_val_speaker_[15]_accuracy_0000,76786.pth'  # path to pretrained model
 cfg.emotion_mapping = {
     'W': 'anger',
     'L': 'boredom',
@@ -18,11 +19,17 @@ cfg.emotion_mapping = {
 cfg.emotions = list(cfg.emotion_mapping.values())
 cfg.num_classes = len(cfg.emotions)
 cfg.num_epochs = 400  # training 400 epochs takes 17min in RTX
+
+# EMODB has recordings from 10 speakers.
+# We use the recordings of 8 speakers as training set
+#                       of 1 speaker  as val set
+#                       of 1 speaker  as test set
 cfg.speaker_assign = {
+    # "all_emodb_speakers": [3, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     "train": [3, 8, 9, 10, 11, 12, 13, 14],
     "val": [15],
-    "test": [16],
-    # "all_speakers": [3, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    "test": [16]
 }
+
 cfg.name2id = dict(zip(cfg.emotions, range(cfg.num_classes)))
 cfg.id2name = dict(zip(range(cfg.num_classes), cfg.emotions))
