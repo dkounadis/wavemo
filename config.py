304d5b4393b8f3de4e899eaf59de47867336be3a
dio <dionyssos.kounadis-bastian@smartmeup.org>
2021-10-28 12:19:36 +0200

    OK config

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import torch
 
 cfg = PermissiveDict()
 cfg.dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-cfg.data_dir = 'emodb-src/'  # path to EMODB http://emodb.bilderbar.info/download/
+cfg.data_dir = 'emodb-src/'  # http://emodb.bilderbar.info/download/
 cfg.net_dir = './networks/'
 
-cfg.pth = 'epoch_138_val_speaker_[15]_accuracy_0000,76786.pth'
+cfg.pth = 'epoch_138_val_speaker_[15]_accuracy_0000,76786.pth'  # used in demo.ipynb
+
 cfg.emotion_mapping = {
     'W': 'anger',
     'L': 'boredom',
@@ -18,17 +19,14 @@ cfg.emotion_mapping = {
 }
 cfg.emotions = list(cfg.emotion_mapping.values())
 cfg.num_classes = len(cfg.emotions)
-cfg.num_epochs = 400  # training 400 epochs takes 17min in RTX
+cfg.num_epochs = 4  # train 400 epochs takes ~2hours in a small GPU
 
-# EMODB has recordings from 10 speakers.
-# We use the recordings of 8 speakers as training set
-#                       of 1 speaker  as val set
-#                       of 1 speaker  as test set
+# EMODB contains recordings from 10 speakers. We use the recordings 
+# of 9 speakers as train and 1 speaker for val
 cfg.speaker_assign = {
     # "all_emodb_speakers": [3, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    "train": [3, 8, 9, 10, 11, 12, 13, 14],
-    "val": [15],
-    "test": [16]
+    "train": [3, 8, 9, 10, 11, 12, 13, 14, 16],
+    "val": [15]
 }
 
 cfg.name2id = dict(zip(cfg.emotions, range(cfg.num_classes)))
